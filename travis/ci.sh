5f6c9c2b344fa057cb8120cab79448ebd20201f0
krakjoe <joe.watkins@live.co.uk>
2014-04-14 09:46:58 +0100

    travis paths

--- travis/ci.sh
+++ travis/ci.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
+###################################################################################
+#  First install prerequisite libraries for php.defaults not available on travis  #
+###################################################################################
 sudo apt-get install -qq libmcrypt-dev libXpm-dev libpng-dev libjpeg-dev libvpx-dev
-mkdir php-build/master
-sudo ./php.multi
-source ./php.env master
-
+###################################################################################
+CWD=$(pwd)
+###################################################################################
+# Run a test build of php-src/master in all configurations
+###################################################################################
+mkdir  php-build/master
+sudo   $CWD/php.multi
+###################################################################################
+# List all configurations
+###################################################################################
+source $CWD/php.env list
