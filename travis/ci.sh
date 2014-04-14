62ec0df82e8d655e676cbb18a7a1339d06eafcee
krakjoe <joe.watkins@live.co.uk>
2014-04-14 09:28:48 +0100

    moar libs

--- travis/ci.sh
+++ travis/ci.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-sudo apt-get install -qq libmcrypt-dev libXpm-dev libpng-dev libjpeg-dev
+sudo apt-get install -qq libmcrypt-dev libXpm-dev libpng-dev libjpeg-dev libvpx-dev
 mkdir php-build/master
 sudo ./php.multi
 source ./php.env master
