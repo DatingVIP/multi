278317553f0f5a5dd9737e20ef567a4c04e33b30
Boris Momčilović <boris@firstbeatmedia.com>
2015-11-05 00:40:08 +0100

    .travis.yml: before_install

--- travis/ci.sh
+++ travis/ci.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 ###################################################################################
-#  First install prerequisite libraries for php.defaults not available on travis  #
-###################################################################################
-sudo add-apt-repository ppa:travis-ci/memcached-sasl
-sudo apt-get update 
-sudo apt-get install -qq libmcrypt-dev libXpm-dev libpng-dev libjpeg-dev libvpx-dev re2c libmemcached-dev
-###################################################################################
 # Run a test build of php-src/master in all configurations
 ###################################################################################
 mkdir /home/travis/build/DatingVIP/multi/php-build/master
