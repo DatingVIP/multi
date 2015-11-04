7f4f465bfbf40daa8bd94b5bd948d4230f3b1e48
Boris Momčilović <boris@firstbeatmedia.com>
2015-11-05 00:34:26 +0100

    travis: ci.sh: memcached sasl

--- travis/ci.sh
+++ travis/ci.sh
@@ -2,6 +2,8 @@
 ###################################################################################
 #  First install prerequisite libraries for php.defaults not available on travis  #
 ###################################################################################
+sudo add-apt-repository ppa:travis-ci/memcached-sasl
+sudo apt-get update 
 sudo apt-get install -qq libmcrypt-dev libXpm-dev libpng-dev libjpeg-dev libvpx-dev re2c libmemcached-dev
 ###################################################################################
 # Run a test build of php-src/master in all configurations
