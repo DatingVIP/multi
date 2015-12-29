090826a54cbcc8f6b568ca02865d79a7ae71248c
Boris Momčilović <boris@firstbeatmedia.com>
2015-12-29 16:41:36 +0100

    hooks: post.install: pthreads use 2.0.10 for 5, github for 7

--- hooks/default/post.install/1000-pthreads.sh
+++ hooks/default/post.install/1000-pthreads.sh
@@ -9,13 +9,13 @@ fi
 if [ $vernum -lt 70000 ]; then
 	__msg "Installing pthreads for $VERSION $ZTS $DBG from PECL"
 	$TARGET/bin/pecl uninstall pthreads           2>&1 >/dev/null
-	$TARGET/bin/pecl install pthreads            2>&1 >/dev/null
+	$TARGET/bin/pecl install pthreads-2.0.10      2>&1 >/dev/null
 	echo extension=pthreads.so                    >$TARGET/modules.d/pthreads.ini
 else
 	__msg "Installing pthreads for $VERSION $ZTS $DBG from github"
 	git clone https://github.com/krakjoe/pthreads		2>&1 >/dev/null
 	cd pthreads
-	git checkout seven
+	git checkout master
 	$TARGET/bin/phpize					2>&1 >/dev/null
 	./configure --with-php-config=$TARGET/bin/php-config	2>&1 >/dev/null
 	make install						2>&1 >/dev/null
